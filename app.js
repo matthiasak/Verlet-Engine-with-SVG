7b8497ae461347ca6676002aa0944225b7266db8
Matt Keas <matthiasak@gmail.com>
2014-02-12 10:25:01 -0600

    adding polyfills

--- app.js
+++ app.js
@@ -1,3 +1,59 @@
+//requestAnimationFrame polyfill
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+//addEventListener polyfill 1.0 / Eirik Backer / MIT Licence
+(function(win, doc){
+    if(win.addEventListener)return;     //No need to polyfill
+
+    function docHijack(p){var old = doc[p];doc[p] = function(v){return addListen(old(v))}}
+    function addEvent(on, fn, self){
+        return (self = this).attachEvent('on' + on, function(e){
+            var e = e || win.event;
+            e.preventDefault  = e.preventDefault  || function(){e.returnValue = false}
+            e.stopPropagation = e.stopPropagation || function(){e.cancelBubble = true}
+            fn.call(self, e);
+        });
+    }
+    function addListen(obj, i){
+        if(i = obj.length)while(i--)obj[i].addEventListener = addEvent;
+        else obj.addEventListener = addEvent;
+        return obj;
+    }
+
+    addListen([doc, win]);
+    if('Element' in win)win.Element.prototype.addEventListener = addEvent;          //IE8
+    else{       //IE < 8
+        doc.attachEvent('onreadystatechange', function(){addListen(doc.all)});      //Make sure we also init at domReady
+        docHijack('getElementsByTagName');
+        docHijack('getElementById');
+        docHijack('createElement');
+        addListen(doc.all);
+    }
+})(window, document);
+
 function Color() {}
 
 Color.prototype.hexToRGB = function(hex) {
